8e79468df9868147558fd7358da4090ef4ea8927
Carlos Ariza <ariza397@hotmail.com>
2021-09-11 09:02:48 -0500

    prueba 2

--- prueba.py
+++ prueba.py
@@ -4,7 +4,7 @@
 #hola mundo es una prueba
 #hola mundo esun prueba
 #hola mundo s una prueba
-#hola mundo es una prueba
+#hola mundo es una pra
 #hola mundo es un prueba
 #hola mundo es una prueba
 #hola mundo es una prueba
